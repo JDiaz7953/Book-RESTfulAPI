f83517207b515a6398769c04fe84044332001cc1
Jaiden <113065377+JDiaz7953@users.noreply.github.com>
2025-02-13 16:31:38 -0500

    Added DELETE, POST, GET Request

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') 
+const Books = require('./model/books')
 
-const URI = 'mongodb+srv://BookUser:book123@cluster0.s1zjw.mongodb.net/Book?retryWrites=true&w=majority&appName=Cluster0'
+const URI = 'mongodb+srv://BookUser:book123@cluster0.s1zjw.mongodb.net/book?retryWrites=true&w=majority&appName=Cluster0'
 
 //connects to the MongoDB database
 mongoose.connect(URI).then(() => {
@@ -10,6 +11,37 @@ mongoose.connect(URI).then(() => {
     console.log('listening on port 3000')
 }).catch(() => console.log('Error connecting to port 3000'))
 
-app.get('/', (req, res) => {
-    res.send('We have Communication')
+app.use(express.json());
+
+
+app.get('/books', (req, res) => {
+    Books.find().then((result) => {
+        res.send(result)
+    })
+})
+
+
+
+app.post('/books', (req,res) =>{
+    const book = new Books(req.body)
+    book.save();
+    res.send(book)
+
+})
+
+app.delete('/books/:id', (req, res) =>{
+    const id = req.params.id;
+    Books.findByIdAndDelete(id).then(() => {
+        console.log('Book Deleted')
+        res.json({message: 'Delete Succesfully'})
+    }).catch(() => {
+        console.log('Issue Unkown')
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+    const id = req.params.id
+    Books.findById(id).then((result) => {
+        res.send(result)
+    })
 })
